323c4c7862d68075a736848c45158d1ceab938a6
htaphuong <htaphuong@apcs.vn>
2017-04-06 02:51:06 +0700

    change package name "test"

---
+++ src/test/test/MockHttpRequest.java
@@ -0,0 +1,11 @@
+import server.CaptchaRequester;
+
+/**
+ * Created by admin on 4/3/2017.
+ */
+public class MockHttpRequest implements CaptchaRequester {
+
+    public String request() {
+        return "";
+    }
+}
