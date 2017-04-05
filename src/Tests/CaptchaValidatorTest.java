c8b9c4b074b7837a1b22212a9872949f78e343f7
htaphuong <htaphuong@apcs.vn>
2017-04-05 21:53:21 +0700

    Edit test source-code

--- src/Tests/CaptchaValidatorTest.java
+++ src/Tests/CaptchaValidatorTest.java
@@ -6,52 +6,52 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Created by admin on 4/3/2017.
  */
-class CaptchaValidatorTest {
+public class CaptchaValidatorTest {
     @Test
-    void isValidate_expectedCaptchaIsNull_ReturnFalse() {
+    public void isValidate_expectedCaptchaIsNull_ReturnFalse() {
         CaptchaValidator cv = new CaptchaValidator(new MockHttpRequest());
 
         assertFalse(cv.isValidate("12345", null));
     }
     @Test
-    void isValidate_receivedCaptchaNull_ReturnFalse() {
+    public void isValidate_receivedCaptchaNull_ReturnFalse() {
         CaptchaValidator cv = new CaptchaValidator(new MockHttpRequest());
 
         assertFalse(cv.isValidate(null, "12345"));
     }
     @Test
-    void isValidate_receivedCaptcha_Equals_ExpectedCaptcha_ReturnTrue() {
+    public void isValidate_receivedCaptcha_Equals_ExpectedCaptcha_ReturnTrue() {
         CaptchaValidator cv = new CaptchaValidator(new MockHttpRequest());
 
         assertTrue(cv.isValidate("12345", "12345"));
     }
 
     @Test
-    void isValidate_receivedCaptcha_DifferentFrom_ExpectedCaptcha_ReturnFalse() {
+    public void isValidate_receivedCaptcha_DifferentFrom_ExpectedCaptcha_ReturnFalse() {
         CaptchaValidator cv = new CaptchaValidator(new MockHttpRequest());
 
         assertFalse(cv.isValidate("12345", "123"));
     }
     @Test
-    void isValidate_receivedCaptcha_isUpperCase_ReturnFalse() {
+    public void isValidate_receivedCaptcha_isUpperCase_ReturnFalse() {
         CaptchaValidator cv = new CaptchaValidator(new MockHttpRequest());
 
         assertFalse(cv.isValidate("ABCD", "abcd"));
     }
     @Test
-    void isValidate_expectedCaptcha_isUpperCase_ReturnFalse() {
+    public void isValidate_expectedCaptcha_isUpperCase_ReturnFalse() {
         CaptchaValidator cv = new CaptchaValidator(new MockHttpRequest());
 
         assertFalse(cv.isValidate("abcd", "ABCD"));
     }
     @Test
-    void isValidate_expectedCaptcha_someCharIsUpperCase_ReturnFalse() {
+    public void isValidate_expectedCaptcha_someCharIsUpperCase_ReturnFalse() {
         CaptchaValidator cv = new CaptchaValidator(new MockHttpRequest());
 
         assertFalse(cv.isValidate("abcd", "aBCb"));
     }
     @Test
-    void isValidate_receivedCaptcha_someCharIsUpperCase_ReturnFalse() {
+    public void isValidate_receivedCaptcha_someCharIsUpperCase_ReturnFalse() {
         CaptchaValidator cv = new CaptchaValidator(new MockHttpRequest());
 
         assertFalse(cv.isValidate("aBCd", "abcd"));
