9a6e87592a1ac81f3850b5b45e1e595d2bb67c3e
htaphuong <htaphuong@apcs.vn>
2017-04-06 03:36:04 +0700

    Update pom + Change test case

--- src/test/test/captchaValidatorTest.java
+++ src/test/test/captchaValidatorTest.java
@@ -57,6 +57,6 @@ public class captchaValidatorTest {
     public void isValidate_receivedCaptcha_someCharIsUpperCase_ReturnFalse() {
         CaptchaValidator cv = new CaptchaValidator(new MockHttpRequest());
 
-        Assert.assertTrue(cv.isValidate("aBCd", "abcd"));
+        Assert.assertFalse(cv.isValidate("aBCd", "abcd"));
     }
 }
